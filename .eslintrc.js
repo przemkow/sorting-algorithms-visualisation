2eead1e77e58372e878221cd58af1e8fcdd4ac77
Przemysław Fałowski <przemkow92@gmail.com>
2020-10-28 00:02:03 +0100

    chore: add tsconfig and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {},
+};
